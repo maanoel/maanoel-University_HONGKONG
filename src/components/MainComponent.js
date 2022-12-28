3a93a5acb9bc0d2035e508eed839b43077c45e42
maanoel <manoelvitorbrito@gmail.com>
2022-12-28 19:11:01 -0300

    [FEAT] adding header and footer components to view

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,6 +3,8 @@ import { Navbar, NavbarBrand } from "reactstrap";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishdetailComponent";
 import { DISHES } from "../shared/dishes";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 
 class Main extends Component {
   constructor(props) {
@@ -20,10 +22,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-        </Navbar>
-
+        <Header />
         <div className="container">
           <Menu
             dishes={this.state.dishes}
@@ -37,6 +36,7 @@ class Main extends Component {
             }
           />
         </div>
+        <Footer />
       </div>
     );
   }
